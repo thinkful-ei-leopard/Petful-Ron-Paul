749fc37cfc6591cbd47d9bdbc07e24af6b20ca53
Ron Martin <ronmartin89@gmail.com>
2020-04-10 11:18:41 -0700

    Built the base Queue with enqueue, dequeue"

--- petful-server-template/modules/queue/Queue.js
+++ petful-server-template/modules/queue/Queue.js
@@ -1,14 +1,37 @@
+class _Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
-  constructor() {
-    // Set initial data.
+  constructor(value){
+    this.first = null;
+    this.last = null;
   }
-
   enqueue(data) {
-    // Add some data to the queue.
+    const node = new _Node(data);
+    if (this.first === null) {
+      this.first = node;
+    }
+    if(this.last){
+      this.last.next = node;
+    }
+    this.last = node;
   }
-
-  dequeue() {
-    // Remove some data from the queue.
+  dequeue(){
+    if(!this.first) {
+      return console.log('nada in queue')
+    }
+    const node = this.first;
+    this.first = this.first.next;
+    if (node === this.last) {
+      this.last = null;
+      return node.value;
+    }
+            
+    return node.value;
   }
 
   show() {
