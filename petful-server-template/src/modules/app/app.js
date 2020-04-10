2783abd4087e81bf72c892041d282391031f55a8
Ron Martin <ronmartin89@gmail.com>
2020-04-10 11:38:53 -0700

    added semi-colons to everything and setup config.js

--- petful-server-template/src/modules/app/app.js
+++ petful-server-template/src/modules/app/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const { NODE_ENV } = require('./config');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
+app.use('/api/people', require('../people/people.router'));
+app.use('/api/pets', require('../pets/pets.router'));
 
-module.exports = app
+app.use(function errorHandler(error, req, res, next) { // eslint-disable-line no-unused-vars
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error'}};
+  } else {
+    console.error(error);
+    response = { message: error.message, error};
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
