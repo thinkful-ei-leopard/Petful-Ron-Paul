fca9ee182d2ea6cce2df9dd694e30a878057e8df
wavinginspace <paulbaisley@gmail.com>
2020-04-10 14:13:22 -0400

    landing page style done

--- petful-client-template/src/root/Root.js
+++ petful-client-template/src/root/Root.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Router, Link } from 'react-router-dom';
+import './Root.scss';
 
 function Root() {
   return (
-    <div>
-      <h1>Welcome to Petful!</h1>
+    <div className="Root">
+      <h1 className="header">Welcome to Petful!</h1>
 
-      <img src="../../images/dogcat.jpeg" alt="Dog and Cat" />
+      <img
+        className="dogcat"
+        src={require('../images/dogcat.jpg')}
+        alt="Dog Cat"
+      />
 
       <p>
-        We operate on a first-come, first-served basis. If you would like to
-        adopt one of our furry friends, click the button below and add your name
-        to the adoption list! Cats and dogs who have been with us the longest
-        will be the first ones available for adoption.
+        We operate on a{' '}
+        <span className="first-come">first-come, first-served</span> basis. If
+        you would like to adopt one of our furry friends, click the button below
+        and add your name to the adoption list! Cats and dogs who have been with
+        us the longest will be the first ones available for adoption.
       </p>
 
-      <Link to="/adoption"></Link>
+      <Link to="/adoption" className="adopt-button">
+        <p>
+        	Adopt Now!
+        </p>
+      </Link>
     </div>
   );
 }
