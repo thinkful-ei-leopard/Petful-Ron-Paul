7d4712a0a38212f08d18e937b24b110cb3dfb287
Ron Martin <ronmartin89@gmail.com>
2020-04-10 20:17:22 -0700

    finishing up /api/pets DELETE endpoint

--- petful-server-template/README.md
+++ petful-server-template/README.md
@@ -14,9 +14,7 @@ returns back with the name of that you sent in.
 Returns a JSON list of all pets. There are two keys "dogs" and "cats". The key's value is an array of objects - each object has all of the details of one pet.
 
 - DELETE
-
-#### /api/pets/dogs
-- DELETE
-
-#### /api/pets/cats
-- DELETE
+Acceptable Input: 
+* 'type' - the type of animal to delete. Valid types are 'cats' or 'dogs', both are strings. We will dequeue the first dog or cat and dequeue the person who adopted the animal
+ 
+* 'both' - boolean, if set to 'true' we will dequeue the first dog AND cat and dequeue the person who adopted the animal
--- petful-server-template/src/modules/pets/pets.router.js
+++ petful-server-template/src/modules/pets/pets.router.js
@@ -16,19 +16,47 @@ router.delete('/', json, (req, res) => {
   // Remove 2 pets (cat and dog) from adoption
   // Remove the person who adopted the pet as well
 
-});
+  // to dequeue people, it requires no input
+  // to dequeue
+  const { type, both } = req.body;
+  
+  // Since 'both' the boolean gets converted to a string, this way we can check 
+  // if the boolean true
+  if(both === 'true'){
+    Pets.dequeue('cats');
+    Pets.dequeue('dogs');
+    People.dequeue();
+    return res.status(204).send();
+  }
+
+  // check if "both" is TRUE first, if it is then do a  dequeue for both types individually
+  // be CAREFUL not to do extra dequeues if we get "type: cats, both: true" - we dont want to delete 2 cats and 1 dog
+  /////////////////// return res.status(200).json({message: 'i dunno bro'});
 
 
-router.delete('/cats', json, (req, res) => {
-  // Remove a pet(cat) from adoption
-  // Remove the person who adopted the pet as well
+  // Validation
+  if(type == null || !type){
+    return res.status(400).json({
+      error: `Invalid 'type'`
+    });
+  }
+  if(typeof type !== 'string'){
+    return res.status(400).json({
+      error: `'type' must be a string`
+    });
+  }
+  if(type !== 'cats' || type !== 'dogs'){
+    return res.status(400).json({
+      error: `'type' must either be 'cats' or 'dogs'`
+    });
+  }
 
-});
+  // In case something doesn't get caught
+  const petType = `${type}`;
+  Pets.dequeue(petType);
+  People.dequeue();
 
-router.delete('/dogs', json, (req, res) => {
-  // Remove a pet(dog) from adoption
-  // Remove the person who adopted the pet as well
-
+  return res.status(204).send();
 });
 
 module.exports = router;
