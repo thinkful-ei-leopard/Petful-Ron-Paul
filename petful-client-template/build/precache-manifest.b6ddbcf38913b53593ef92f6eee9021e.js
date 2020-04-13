5abcfe430ecf5f945fc819482ff3d773c7249168
Ron Martin <ronmartin89@gmail.com>
2020-04-13 16:39:23 -0700

    updated for deploy

---
+++ petful-client-template/build/precache-manifest.b6ddbcf38913b53593ef92f6eee9021e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fc994b2cc59aaf6dfe119621d7999d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41394b65b2428e2e5025",
+    "url": "/static/css/main.37ec2d17.chunk.css"
+  },
+  {
+    "revision": "18bee03cb21f06de9ffe",
+    "url": "/static/js/2.30049ee2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.30049ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41394b65b2428e2e5025",
+    "url": "/static/js/main.101d5a05.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "723fa51c615e7ebce4c5690bdd67988a",
+    "url": "/static/media/dogcat.723fa51c.jpg"
+  }
+]);
